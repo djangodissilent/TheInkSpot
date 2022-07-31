815c52bd2fa604a2f0b0665482edb44ae22e2a84
rslan <mrdjangodev@gmail.com>
2022-07-31 21:39:20 +0200

    added the follow APIView

--- theinkspot/users/api/serializers.py
+++ theinkspot/users/api/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from theinkspot.users.models import UserFollow
 
 User = get_user_model()
 
@@ -12,3 +14,17 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
         }
+
+
+class FollowedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserFollow
+        fields = ("id", "follower_user", "created")
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserFollow
+        fields = ("id", "followed_user", "created")
+
+
--- theinkspot/users/api/views.py
+++ theinkspot/users/api/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.views import APIView
+
 
 from .serializers import UserSerializer
 
@@ -23,3 +25,44 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     def me(self, request):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+
+class FollowUser(APIView):
+    def post(self, request, format=None):
+        username = request.data.get("username")
+        if username is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user_follow = UserFollowing(follower_user=request.user, followed_user=user)
+        try:
+            user_follow.save()
+        except ValueError as ve:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"message": ve.args[0]})
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class UnfollowUser(APIView):
+    def post(self, request, format=None):
+        username = request.data.get("username")
+        if username is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            user_follow = UserFollowing.objects.filter(follower_user=request.user, followed_user=user)
+
+        except UserFollowing.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        try:
+            user_follow.delete()
+        except ValueError as ve:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"message": ve.args[0]})
+
+        return Response(status=status.HTTP_201_CREATED)
